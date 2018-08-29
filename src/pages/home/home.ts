7ab1d721e4744cdf80fda65f8ebb20a7965d67a3
yanchenru <yanchenru@gmail.com>
2018-08-29 01:08:01 -0600

    Add validator

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,57 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private eventForm: FormGroup;
+  //private eventDuration = false;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {
+    console.log('const')
+    this.eventForm = this.formBuilder.group({
+      pickEventStartDate: ['', Validators.required],
+      pickEventEndDate: ['', Validators.required],
+      pickEventStartTime: ['', Validators.required],
+      pickEventFinishTime: ['', Validators.required],
+      address: ['', Validators.required],
+      proximity: ['', Validators.required],
+    }, {validator: this.dateLessThan("pickEventStartDate", "pickEventEndDate", "pickEventStartTime", "pickEventFinishTime")});
   }
 
+  dateLessThan(startDate: string, endDate: string, startTime:string, finishTime:string) {
+    return (group: FormGroup): {[key: string]: any} => {
+     let sd = group.controls[startDate];
+     let ed = group.controls[endDate];
+     let st = group.controls[startTime];
+     let ft = group.controls[finishTime];
+     if (sd.value > ed.value) {
+       return {
+         dates: "Start Date should be equal or less than End Date"
+       };
+     }
+     else if (sd.value === ed.value){
+       if (st.value > ft.value){
+        return {
+          time: "Start Time should be equal or less than Finish Time"
+        };
+       }
+     }
+     return {};
+    }
+  }
+
+  ngOnInit(){
+    console.log('init')
+  }
+
+  create() {
+    //console.log(this.pickEventStartDate, this.pickEventEndDate, this.pickEventStartTime, this.pickEventFinishTime, this.address, this.proximity);
+    console.log(this.eventForm.value)
+
+  }
 }
