9d959eb7d8406802743d1dedde0f7a4222cc5170
Chenru Yan <yanchenru@gmail.com>
2019-01-14 02:03:02 -0700

    test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,7 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import firebase from 'firebase';
 
 declare var placeSearch;
 
